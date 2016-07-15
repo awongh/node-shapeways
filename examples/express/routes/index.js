5ecee6141dd2e44060a5bc5ebc3145d68fa3f810
akira <akira@awonghs-MacBook-Pro.local>
2016-07-14 20:15:02 -0400

    example of unparsable error message

--- examples/express/routes/index.js
+++ examples/express/routes/index.js
@@ -1,3 +1,4 @@
+fs = require('fs');
 
 /*
  * GET home page.
@@ -5,10 +6,40 @@
 var config = require('../config.json');
 
 exports.index = function(req, res){
-    req.api.getApiInfo(function(error, results){
-        res.render('index', {
-            title: config.app.name,
-            results: results,
-        });
+
+    // read an stl file in from the current dir
+    fs.readFile('example.stl', 'utf8', function (err,filestrr) {
+
+      if (err) {
+        return console.log(err);
+      }
+
+      //assign a filename that will error out
+      var filename = "example"; //example.stl will work error free
+
+      var params = {
+        "file":filestrr,
+        "fileName":filename,
+        "hasRightsToModel":1,
+        "acceptTermsAndConditions":1
+      };
+
+      req.api.addModel(params, function(error, results){
+
+          if( error != null ){
+
+            //error.data is not a JSON object
+            var error_json = JSON.parse( error.data );
+
+            //other stuff that needs to happen with error.data
+          }
+
+          res.render('index', {
+              title: config.app.name,
+              results: results
+          });
+      });
+
     });
+
 };
